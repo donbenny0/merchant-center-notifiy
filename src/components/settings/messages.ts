a50f3e2e78305c8219132a63d10ebd661958da7f
don benny <don.benny@ayatacommerce.com>
2024-11-08 13:25:59 +0530

    update : add view switcher

--- src/components/settings/messages.ts
+++ src/components/settings/messages.ts
@@ -2,17 +2,17 @@ import { defineMessages } from 'react-intl';
 
 export default defineMessages({
     backToWelcome: {
-        id: 'editMessages.backToWelcome',
+        id: 'settings.backToWelcome',
         defaultMessage: 'Back to Notifications',
     },
     title: {
-        id: 'editMessages.title',
-        defaultMessage: 'Edit Messages',
+        id: 'settings.title',
+        defaultMessage: 'Settings',
     },
     subtitle: {
-        id: 'editMessages.subtitle',
+        id: 'settings.subtitle',
         defaultMessage:
-            'You can edit the content of your message body in this section. Feel free to customize your placeholders and enhance the message to better suit your needs.',
+            'You can configure notification settings. Edit message template etc.',
     },
    
 });
--- src/components/settings/settings.tsx
+++ src/components/settings/settings.tsx
@@ -18,7 +18,8 @@ import { validateTemplate } from "../../utils/messageBody.utils";
 import { useShowNotification } from '@commercetools-frontend/actions-global';
 import { useIntl } from 'react-intl';
 import SelectField from "@commercetools-uikit/select-field";
-import {generateMessage } from "../../utils/messageTemplate.utils";
+import { generateMessage } from "../../utils/messageTemplate.utils";
+import ViewSwitcher from '@commercetools-uikit/view-switcher';
 type TEditMessagesProps = {
     linkToNotifications: string;
 };
@@ -34,7 +35,7 @@ const EditMessages = ({ linkToNotifications }: TEditMessagesProps) => {
     const [showPreview, setPreview] = useState(false);
     const [orders, setOrders] = useState<any[]>([]);
     const [renderedMessage, setRenderedMessage] = useState('');
-
+    const [seletedValue, setSelectedValue] = useState('edit-template');
 
     const dispatch = useAsyncDispatch();
     const intl = useIntl();
@@ -144,99 +145,110 @@ const EditMessages = ({ linkToNotifications }: TEditMessagesProps) => {
                         <Text.Subheadline as="h5" intlMessage={messages.subtitle} />
                     </Spacings.Stack>
 
-                    <Spacings.Stack scale="s">
-                        <Card theme="light" type="raised">
-                            <div className={styles.actionButtons}>
-                                <div>
+                    <ViewSwitcher.Group
+                        selectedValue={seletedValue}
+                        onChange={setSelectedValue}
+                    >
+                        <ViewSwitcher.Button isDisabled={seletedValue === 'edit-template'} value="edit-template">
+                            Edit Template
+                        </ViewSwitcher.Button>
+                    </ViewSwitcher.Group>
+
+                    {seletedValue === 'edit-template' && (
+                        <Spacings.Stack scale="s">
+                            <Card theme="light" type="raised">
+                                <div className={styles.actionButtons}>
+                                    <div>
+                                        <button
+                                            onClick={() => setSelectedMethod("whatsapp")}
+                                            className={selectedMethod === "whatsapp" ? styles.active : ""}>
+                                            <img alt="web developer" src={whatsappSvg} />
+                                            <span>WhatsApp</span>
+                                        </button>
+                                    </div>
+
+                                    <div className={styles.rightSideButtons}>
+                                        <FlatButton icon={<InformationIcon />} onClick={() => handleNotes(!showNotes)} label="Show guidelines" />
+                                        {editedMessage === '' ? (<></>) : (<FlatButton icon={showPreview ? <EyeCrossedIcon /> : <EyeIcon />} onClick={() => handlePreview(!showPreview)} label={showPreview ? 'Hide preview' : 'Preview'} />)}
+                                    </div>
+                                </div>
+
+                                {showNotes && (
+                                    <Card theme="light" type="raised" className={styles.noteContainer}>
+                                        <h4>Note :</h4>
+                                        <ul style={{ lineHeight: '1.5' }}>
+                                            <li><b>Mandatory Fields</b>: Ensure all placeholders are populated with valid data from the <Link isExternal={true} to={"https://docs.commercetools.com/api/projects/orders#order"}>order</Link> response to avoid incomplete messages. </li>
+                                            <li><b>Character Limits</b>: Consider WhatsApp's character limits (4096 characters for messages) to prevent truncation.</li>
+                                            <li><b>Dynamic Data</b>: Ensure the order object has necessary attributes (e.g., shippingAddress, id, totalPrice) populated before generating the message.</li>
+                                            <li><b>Supported resource</b>: Currently, this application supports only the order resource. Please refer <Link isExternal={true} to={"https://docs.commercetools.com/api/projects/orders#order"}>CommerceTools official documentation</Link> for order resource and its attributes for more details.</li>
+                                        </ul>
+                                    </Card>
+                                )}
+
+                                {showPreview && (
+                                    <Card theme="light" type="raised" className={styles.noteContainer}>
+                                        <SelectField
+                                            title="Select your order"
+                                            name="order-select"
+                                            value={{
+                                                label: 'Default Template',
+                                                value: 'default'
+                                            }}
+                                            options={orders.map(order => ({
+                                                label: order.lineItems.map((item: any) => item.variant.sku).join(', '),
+                                                value: order.id
+                                            }))}
+                                            onChange={(event) => {
+                                                // Handle template selection
+                                                handlePreviewRender(orders, editedMessage, event.target.value as string)
+                                            }}
+                                        />
+                                        {renderedMessage !== '' ? (
+                                            <div className={styles.previewContainer}>
+                                                <h4>Preview :</h4>
+                                                <br />
+                                                <p dangerouslySetInnerHTML={{ __html: renderedMessage.replace(/\n/g, '<br />') }}></p>
+                                            </div>
+
+                                        ) : null}
+                                    </Card>
+                                )}
+
+                                <div className={styles.messageArea}>
+                                    <MultilineTextField
+                                        title="Message body"
+                                        placeholder="What's your message body"
+                                        value={editedMessage}
+                                        onFocus={() => {
+                                            setPreview(false)
+                                            setRenderedMessage('')
+                                        }}
+                                        defaultExpandMultilineText={true}
+
+                                        onChange={handleTemplateChange}
+                                        id="messageBodyTextarea"
+                                    />
+                                    {validationErrors.length > 0 && (
+                                        <div className={styles.validationErrors}>
+                                            <ul>
+                                                {validationErrors.map((error, index) => (
+                                                    <li key={index}>{error}</li>
+                                                ))}
+                                            </ul>
+                                        </div>
+                                    )}
+                                </div>
+                                <div className={styles.cardFooter}>
                                     <button
-                                        onClick={() => setSelectedMethod("whatsapp")}
-                                        className={selectedMethod === "whatsapp" ? styles.active : ""}>
-                                        <img alt="web developer" src={whatsappSvg} />
-                                        <span>WhatsApp</span>
+                                        onClick={handleSave}
+                                        disabled={isSaving || validationErrors.length > 0}
+                                    >
+                                        {isSaving ? <div className={styles.loader}></div> : 'Save changes'}
                                     </button>
                                 </div>
-
-                                <div className={styles.rightSideButtons}>
-                                    <FlatButton icon={<InformationIcon />} onClick={() => handleNotes(!showNotes)} label="Show guidelines" />
-                                    {editedMessage === '' ? (<></>) : (<FlatButton icon={showPreview ? <EyeCrossedIcon /> : <EyeIcon />} onClick={() => handlePreview(!showPreview)} label={showPreview ? 'Hide preview' : 'Preview'} />)}
-                                </div>
-                            </div>
-
-                            {showNotes && (
-                                <Card theme="light" type="raised" className={styles.noteContainer}>
-                                    <h4>Note :</h4>
-                                    <ul style={{ lineHeight: '1.5' }}>
-                                        <li><b>Mandatory Fields</b>: Ensure all placeholders are populated with valid data from the <Link isExternal={true} to={"https://docs.commercetools.com/api/projects/orders#order"}>order</Link> response to avoid incomplete messages. </li>
-                                        <li><b>Character Limits</b>: Consider WhatsApp's character limits (4096 characters for messages) to prevent truncation.</li>
-                                        <li><b>Dynamic Data</b>: Ensure the order object has necessary attributes (e.g., shippingAddress, id, totalPrice) populated before generating the message.</li>
-                                        <li><b>Supported resource</b>: Currently, this application supports only the order resource. Please refer <Link isExternal={true} to={"https://docs.commercetools.com/api/projects/orders#order"}>CommerceTools official documentation</Link> for order resource and its attributes for more details.</li>
-                                    </ul>
-                                </Card>
-                            )}
-
-                            {showPreview && (
-                                <Card theme="light" type="raised" className={styles.noteContainer}>
-                                    <SelectField
-                                        title="Select your order"
-                                        name="order-select"
-                                        value={{
-                                            label: 'Default Template',
-                                            value: 'default'
-                                        }}
-                                        options={orders.map(order => ({
-                                            label: order.lineItems.map((item: any) => item.variant.sku).join(', '),
-                                            value: order.id
-                                        }))}
-                                        onChange={(event) => {
-                                            // Handle template selection
-                                            handlePreviewRender(orders, editedMessage, event.target.value as string)
-                                        }}
-                                    />
-                                    {renderedMessage !== '' ? (
-                                        <div className={styles.previewContainer}>
-                                            <h4>Preview :</h4>
-                                            <br />
-                                            <p dangerouslySetInnerHTML={{ __html: renderedMessage.replace(/\n/g, '<br />') }}></p>
-                                        </div>
-
-                                    ) : null}
-                                </Card>
-                            )}
-
-                            <div className={styles.messageArea}>
-                                <MultilineTextField
-                                    title="Message body"
-                                    placeholder="What's your message body"
-                                    value={editedMessage}
-                                    onFocus={() => {
-                                        setPreview(false)
-                                        setRenderedMessage('')
-                                    }}
-                                    defaultExpandMultilineText={true}
-
-                                    onChange={handleTemplateChange}
-                                    id="messageBodyTextarea"
-                                />
-                                {validationErrors.length > 0 && (
-                                    <div className={styles.validationErrors}>
-                                        <ul>
-                                            {validationErrors.map((error, index) => (
-                                                <li key={index}>{error}</li>
-                                            ))}
-                                        </ul>
-                                    </div>
-                                )}
-                            </div>
-                            <div className={styles.cardFooter}>
-                                <button
-                                    onClick={handleSave}
-                                    disabled={isSaving || validationErrors.length > 0}
-                                >
-                                    {isSaving ? <div className={styles.loader}></div> : 'Save changes'}
-                                </button>
-                            </div>
-                        </Card>
-                    </Spacings.Stack>
+                            </Card>
+                        </Spacings.Stack>
+                    )}
                 </>
             )}
         </Spacings.Stack>
